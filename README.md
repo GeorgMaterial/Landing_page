03469a75a9b3d9714ba3cbefef7209b59b51ddce
GeorgMaterial <georgmarley@gmail.com>
2023-05-01 13:24:55 +1000

    update

---
+++ README.md
@@ -0,0 +1,10 @@
+# Landing Page Project
+
+## Project Description
+
+Its a landing page with 4 sections, a navbar and a basic footer. 
+
+## Usage
+
+I wanted to use this as a way to learn more about formatting & dynamics, in a way that I'd be able to use for my music project. So, the 4 sections on the page have a song title, a blurb of what the song is about, and a scrollable section to be able to read the lyrics of the song. Further things I would add if I had more time, would probably be space for some visual elements (videos or images) and probably some links where you could listen to the songs. 
+
--- js/app.js
+++ js/app.js
@@ -1,107 +1,91 @@
 // DEFINING SOME VARIABLES //
 
-const navbar__ul = document.querySelector('#navbar__list') ;
-const sections__el = document.querySelectorAll('section') ;
-const page__header = document.querySelector('.page__header') ;
-const vpHeight = visualViewport.height ;
-const links = [] ;
+const navbar__ul = document.querySelector('#navbar__list')
+const sections__el = document.querySelectorAll('section')
+const page__header = document.querySelector('.page__header')
+const vpHeight = visualViewport.height
+const links = []
 
-let rect = page__header.getBoundingClientRect();
-const header_bottom = rect['bottom'] ;
-
-
+let rect = page__header.getBoundingClientRect()
+const header_bottom = rect['bottom']
 
 // CREATING DYNAMIC NAVBAR //
 
-function getButton(section){
-    let data_attr = section.getAttribute('data-nav') ;
-    let new_li = document.createElement('li') ;
-    new_li.dataset.nav = data_attr ;
-    let button = document.createElement('button') ;
-    button.className = `nav__button` ;
-    button.innerText = data_attr ;
-    button.dataset.section = section.id ; 
-    new_li.appendChild(button) ;
-    navbar__ul.appendChild(new_li) ;
+function getButton (section) {
+  let data_attr = section.getAttribute('data-nav')
+  let new_li = document.createElement('li')
+  new_li.dataset.nav = data_attr
+  let button = document.createElement('button')
+  button.className = `nav__button`
+  button.innerText = data_attr
+  button.dataset.section = section.id
+  new_li.appendChild(button)
+  navbar__ul.appendChild(new_li)
 
-    return button ;
+  return button
 }
 
 // CLASS OF LINKS BETWEEN SECTIONS & NAV BUTTONS //
 
 class Link {
+  constructor (section) {
+    this.nav_button = getButton(section)
+    this.section = section
+    this.lyric_button = section.querySelector('button')
+  }
 
-    constructor(section){
-        this.nav_button = getButton(section) ;
-        this.section = section ;
-        this.lyric_button = section.querySelector('button') ;
-        
+  // DETERMINE IF SECTION IS 'ACTIVE' (IN VIEWPORT/WINDOW) //
+
+  active () {
+    const rect = this.section.getBoundingClientRect()
+    let s_top = rect['top']
+    let s_bottom = rect['bottom']
+    if ((s_top >= header_bottom) & (s_bottom <= vpHeight)) {
+
+      this.nav_button.classList.add('__active')
+      this.section.classList.add('__active')
+
+      return true
+    } else {
+      this.nav_button.classList.remove('__active')
+      this.section.classList.remove('__active')
+
+      return false
     }
-
-    // DETERMINE IF SECTION IS 'ACTIVE' (IN VIEWPORT/WINDOW) //
-
-    active(){
-        const rect = this.section.getBoundingClientRect();
-        let s_top = rect['top'] ;
-        let s_bottom = rect['bottom'] ;
-        if (s_top >= header_bottom & s_bottom <= vpHeight) {
-
-            // Add some math so that it's still active when 90% is on screen //
-
-            this.nav_button.classList.add('__active') ;
-            this.section.classList.add('__active') ;
-
-            return true ;
-
-        } else {
-
-            this.nav_button.classList.remove('__active') ;
-            this.section.classList.remove('__active') ;
-            
-            return false ;
-
-        }
-    }
-
+  }
 }
 
 // creates group of link objects //
 
-for (let section of sections__el){
-    let link = new Link(section)
-    links.push(link) ;
+for (let section of sections__el) {
+  let link = new Link(section)
+  links.push(link)
 
-    // adds scroll on click functionality //
-    link.nav_button.addEventListener('click', _ => {
-        if (link.active() !== true ){
-            link.section.scrollIntoView({block: 'center', behavior:'smooth'});
-        }
-    })
-    
-    link.lyric_button.addEventListener('click', _ => {
-        let lyrics = link.section.querySelector('.lyrics') ;
-        lyrics.toggleAttribute('active');
-        link.lyric_button.toggleAttribute('active');
-        if (lyrics.hasAttribute('active')){
-            link.lyric_button.innerHTML = "<em>Hide Lyrics</em>" ;
-        } else {
-            link.lyric_button.innerHTML = "<em>View Lyrics</em>" ;
-        }
-    })
-}  
+  // adds scroll on click functionality //
+  link.nav_button.addEventListener('click', _ => {
+    if (link.active() !== true) {
+      link.section.scrollIntoView({ block: 'center', behavior: 'smooth' })
+    }
+  })
+
+  link.lyric_button.addEventListener('click', _ => {
+    let lyrics = link.section.querySelector('.lyrics')
+    lyrics.toggleAttribute('active')
+    link.lyric_button.toggleAttribute('active')
+    if (lyrics.hasAttribute('active')) {
+      link.lyric_button.innerHTML = '<em>Hide Lyrics</em>'
+    } else {
+      link.lyric_button.innerHTML = '<em>View Lyrics</em>'
+    }
+  })
+}
 
 // SCROLL EVENT LISTENER //
 
-document.addEventListener("scroll", _ => {
-    for (let link of links ){
-        link.active() ;
-    }
+document.addEventListener('scroll', _ => {
+  for (let link of links) {
+    link.active()
+  }
 
-    // back to top //
-
-});
-
-
-
-
-
+  // back to top //
+})
